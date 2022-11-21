e3e53995833d3e1885c76f18ef996173c4c1cf41
DESKTOP-2LVKBQD\user <mayhy28@chunjae.co.kr>
2022-11-21 11:00:26 +0900

    commit

--- js/script.js
+++ js/script.js
@@ -16,13 +16,15 @@ $(document).ready(function() {
         spaceBetween: 70,
         slidesPerGroup: 1,
         loop: true,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
         navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        breakpoints: {
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 25,
+            }
         },
     });
 });
